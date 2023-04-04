70b5ba6eae95f564b5c423225afaea1882cb7992
kristinabennett <krisbennett@mail.ru>
2023-04-04 10:12:12 +0300

    commit_1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,20 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld secondBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(secondBean.getMessage());
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+
+        Cat secondCatBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(secondCatBean.getMessage());
+
+        System.out.println(bean == secondBean);
+        System.out.println(catBean == secondCatBean);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,38 @@
+import java.util.Objects;
+
+/**
+ * POJO class.
+ */
+public class Cat {
+ private String message;
+
+    public Cat() {
+    }
+
+    public Cat(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(message, cat.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message);
+    }
+}
